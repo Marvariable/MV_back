9d3d9957442f5e4b53f83eec4acb64f64407a36f
Cristina Viejó <krissvinti@gmail.com>
2026-03-19 15:29:33 +0100

    MAR-22 Construccion del punto env

---
+++ src/main/java/com/marvariable/marvariable_spring/MarvariableSpringApplication.java
@@ -0,0 +1,13 @@
+package com.marvariable.marvariable_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvariableSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvariableSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvariable/marvariable_spring/MarvariableSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvariable.marvariable_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvariableSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
